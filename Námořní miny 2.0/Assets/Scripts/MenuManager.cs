a3d2324e42788f6275df3f800c09dbb2118db38d
Aleš Dajčar <Dajcar41@gmail.com>
2023-02-28 18:09:45 +0100

    Zkouška

--- Námořní miny 2.0/Assets/Scripts/MenuManager.cs
+++ Námořní miny 2.0/Assets/Scripts/MenuManager.cs
@@ -5,31 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    //public void Play()
-    //{
-    //    SceneManager.LoadScene(0);
-    //}
-
-    //public void Quit()
-    //{
-    //    Application.Quit();
-    //}
-
-     void Update()
+    public void Play()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                //Select stage    
-                if (hit.transform.name == "Cube")
-                {
-                    SceneManager.LoadScene(0);
-                }
-            }
-        }
+        SceneManager.LoadScene(0);
+        print("jj");
     }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("ukonèení hry");
+    }
+
+    public void OnMOuseUp()
+    {
+        print("ahoj");
+    }
+
+    //public void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        RaycastHit hit;
+    //        if (Physics.Raycast(ray, out hit))
+    //        {
+    //                SceneManager.LoadScene(0);
+                
+    //        }
+    //    }
+    //}
 
 }
